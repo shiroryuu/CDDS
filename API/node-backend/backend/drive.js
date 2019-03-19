b8290e0fce291f96d3f9de5ee058e3b5c23e0df8
Arvind Hariharan Nair <arvdev96@gmail.com>
2019-03-19 12:03:51 +0530

    Add gdocs support

--- API/node-backend/backend/drive.js
+++ API/node-backend/backend/drive.js
@@ -19,9 +19,6 @@ function listFiles(auth,cb) {
       if (files.length) {
         // console.log('Files:');
         return cb(files);
-        // files.map((file) => {
-        //   console.log(`${file.name} (${file.id}) [${file.mimeType}]`);
-        // });
       } else {
         console.log('No files found.');
       }
@@ -29,30 +26,67 @@ function listFiles(auth,cb) {
 }
 
 
+function searchMimeType(auth,fileId,cb){
+  const drive = google.drive('v3');
+  drive.files.get({
+      auth: auth,
+      fileId: fileId,
+  },{responseType: 'json'},
+  function(err, res){
+    return cb(res);
+  });
+}
+
 function downloadFiles(auth,fileId,cb){
     const drive = google.drive('v3');
     const filename = uuid.v4();
     const filepath = path.join(FILES_PATH, filename);
-
     let dest = fs.createWriteStream(filepath);
-    drive.files.get({
+    searchMimeType(auth,fileId,(result)=>{
+      const mimeType = result.data.mimeType.split('/')[1].startsWith("vnd.google-apps");
+      console.log(mimeType);
+      if (mimeType){
+        //download docs
+        drive.files.export({
+          auth: auth,
+          fileId:fileId,
+          mimeType: 'application/pdf'
+        },{responseType: 'stream'},
+        function(err,res){
+          res.data
+          .on('end', () => {
+            console.log('Done downloading document.');
+            return cb({filename,FILES_PATH});
+           
+          })
+          .on('error', err => {
+            console.error('Error downloading document.');
+            
+          })
+          .pipe(dest);
+        });
+      } else {
+        drive.files.get({
         auth: auth,
-        fileId: fileId
-        // alt: 'media'
-    },{responseType: 'stream'},
-    function(err, res){
-      console.log(res);
+        fileId: fileId,
+        alt: 'media'
+        },{responseType: 'stream'},
+        function(err, res){
+        console.log(res);
         res.data
-        .on('end', () => {
+          .on('end', () => {
             console.log('Done');
             return cb({filename,FILES_PATH});
-        })
-        .on('error', err => {
+          })
+          .on('error', err => {
             console.log('Error', err);
             return cb(err);
-        })
-        .pipe(dest);
+          })
+          .pipe(dest);
+        });
+      }
     });
 }
+
 
 module.exports = { listFiles,downloadFiles }
