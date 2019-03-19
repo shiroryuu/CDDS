a623a0475079072a078b558caa887b7757715ef1
Arvind Hariharan Nair <arvdev96@gmail.com>
2019-03-19 12:04:30 +0530

    Add oauth callback

--- API/node-backend/backend/app.js
+++ API/node-backend/backend/app.js
@@ -16,6 +16,17 @@ const SCOPES = ['https://www.googleapis.com/auth/drive'];
 const keyfile = path.join(__dirname, 'credentials.json');
 const keys = JSON.parse(fs.readFileSync(keyfile));
 
+const oAuth2Client = new google.auth.OAuth2(
+    keys.web.client_id,
+    keys.web.client_secret,
+    keys.web.redirect_uris[0]
+);
+
+// const authUrl = oAuth2Client.generateAuthUrl({
+    // access_type: 'offline',
+    // scope: SCOPES,
+// });
+
 //HTTP Headers
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin","*");
@@ -29,17 +40,22 @@ app.use((req,res,next) => {
 });
 
 //Routes
-app.get('/oauth2callback', (req, res) => {
+
+app.get('/oauthcallback', (req, res) => {
   const code = req.query.code;
-  client.getToken(code, (err, tokens) => {
+  console.log(code);
+  oAuth2Client.getToken(code, (err, token) => {
     if (err) {
       console.error('Error getting oAuth tokens:');
       throw err;
     }
-    client.credentials = tokens;
+    oAuth2Client.credentials = token;
+    console.log(token);
     res.send('Authentication successful! Please return to the console.');
-    server.close();
-    drive.listFiles(client);
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+    });
   });
 });
 
@@ -75,7 +91,6 @@ app.get("/files/:file_id",(req,res,next)=>{
   });
 });
 
-
 app.get("/chunks/:filename", (req,res,next)=>{
   const filename = req.params.filename;
   chunk.chunking(filename,(chunk_arr,chunk_hash)=>{
@@ -88,42 +103,42 @@ app.get("/chunks/:filename", (req,res,next)=>{
 
 
 //functions 
-function checkAccessToken(callback,fileID,cb){
-  const {client_secret, client_id, redirect_uris} = keys.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]
-  );
-  
+function checkAccessToken(callback,fileID,cb){  
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
+    // if (err) return getAccessToken(oAuth2Client, callback);
+    if (err) return getAccessToken();
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client,fileID,cb);
   });
 }
 
-function getAccessToken(oAuth2Client, callback){
+// function getAccessToken(oAuth2Client, callback, cb){
+function getAccessToken(){
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  opn(authUrl, {wait: false});
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
+  // console.log('Authorize this app by visiting this url:', authUrl);
+  opn(authUrl, {wait: false} );
+
+  // callback(oAuth2Client)
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  // });
+  // rl.question('Enter the code from that page here: ', (code) => {
+  //   rl.close();
+  //   oAuth2Client.getToken(code, (err, token) => {
+  //     if (err) return console.error('Error retrieving access token', err);
+  //     oAuth2Client.setCredentials(token);
+  //     console.log(token);
+  //     // Store the token to disk for later program executions
+  //     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+  //       if (err) console.error(err);
+  //       console.log('Token stored to', TOKEN_PATH);
+  //     });
+  //     callback(oAuth2Client,cb);
+  //   });
+  // });
 }
 module.exports = app;
